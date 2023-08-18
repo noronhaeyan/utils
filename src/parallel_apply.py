a1c4a44959aa73637f98cd6e2df9dfded0463836
noronhaeyan <noronhaeyan@gmail.com>
2023-08-18 08:54:03 -0400

    First commit

---
+++ src/parallel_apply.py
@@ -0,0 +1,31 @@
+from threading import Thread
+from functools import partial
+import numpy as np
+import pandas as pd
+
+
+def parallelize(data, func, n_jobs=8):
+    data_split = np.array_split(data, n_jobs)
+
+    t = [None] * n_jobs
+
+    output = [pd.DataFrame()] * n_jobs
+
+    for i in range(n_jobs):
+        t[i] = Thread(target=func, args=(data_split[i], output, i, ))
+
+    for i in range(n_jobs):
+        t[i].start()
+
+    for i in range(n_jobs):
+        t[i].join()
+
+    return pd.concat(output, axis=0)
+
+
+def run_on_subset(func, data_subset, output, index):
+    output[index] = data_subset.apply(func, axis=1)
+
+
+def parallelize_on_rows(data, func, n_jobs=8):
+    return parallelize(data, partial(run_on_subset, func), n_jobs=n_jobs)
